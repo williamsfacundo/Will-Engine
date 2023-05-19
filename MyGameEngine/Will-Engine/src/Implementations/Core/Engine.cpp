f9dfd5072ed1847d16e2a2774c360b7da96479d5
Facundo <facuwilli@gmail.com>
2023-05-19 10:40:28 -0300

    Added vertex attributes config functions.

--- MyGameEngine/Will-Engine/src/Implementations/Core/Engine.cpp
+++ MyGameEngine/Will-Engine/src/Implementations/Core/Engine.cpp
@@ -105,6 +105,10 @@ namespace WillEngine
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		
+		glEnableVertexAttribArray(0);
 	}
 
 	void Engine::setVertexShader()
