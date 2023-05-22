5155f9f5a65386a759630d804114e62f471550ef
Facundo <facuwilli@gmail.com>
2023-05-22 13:22:41 -0300

    Added empty input class.

---
+++ MyGameEngine/Will-Engine/src/Headers/Core/input.h
@@ -0,0 +1,16 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+namespace WillEngine
+{
+	class Input
+	{
+	private:
+
+	public:
+		Input();
+		~Input();
+	};
+}
+
+#endif
---
+++ MyGameEngine/Will-Engine/src/Implementations/Core/input.cpp
@@ -0,0 +1,14 @@
+#include "Core/input.h"
+
+namespace WillEngine
+{
+	Input::Input()
+	{
+
+	}
+
+	Input::~Input()
+	{
+
+	}
+}
